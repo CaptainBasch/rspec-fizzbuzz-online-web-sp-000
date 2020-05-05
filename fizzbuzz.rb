97dc05d5b931d313d7c397cf5863ba3c86de1443
Bailey Davis <bailey.gage.davis@gmail.com>
2020-05-05 16:19:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
-  if input
+  
+  output = ""
+  
+  if input % 3 == 0
+    output = output + "Fizz"
+  end
+  
+  if input % 5 == 0
+    output = output + "Buzz"
+  end
+  
+  output
+  
 end
